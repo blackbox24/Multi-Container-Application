66df48160d09323f9a7cd25595798c3e14466cd4
blackbox24 <nicardid@gmail.com>
2026-02-05 18:18:05 +0000

    chore: added variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_name" {
+  description = "Learn terraform"
+  type = string
+  default = "learn-terraform"
+}
+
+variable "instance_type" {
+    description = "The EC2 instance's type"
+    type = string
+    default = "t2.mirco"
+}
