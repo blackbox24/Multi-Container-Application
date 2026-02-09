a253451dc4504029babeeeef04e746145e7da82d
blackbox24 <nicardid@gmail.com>
2026-02-09 15:56:50 +0000

    chore: modified terraform added security groups [release] v1.3.0

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,51 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
-# resource "aws_instance" "app_server" {
-#   ami           = data.aws_ami.ubuntu.id
-#   instance_type = var.instance_name
+# Security Group definition
+resource "aws_security_group" "allow_traffic" {
+  name        = "allow_ssh_tcp"
+  description = "Allow SSH and standard TCP traffic"
+  # vpc_id    = "your-vpc-id" # Optional: specify if not using default VPC
+}
 
-#   tags = {
-#     Name = var.instance_name
-#   }
-# }
+# Allow SSH (Port 22)
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow_traffic.id
+  cidr_ipv4         = "0.0.0.0/0" # Consider restricting to your IP for better security
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+# Allow HTTP (Port 80)
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.allow_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_port_3000" {
+  security_group_id = aws_security_group.allow_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 3000
+  ip_protocol       = "tcp"
+  to_port           = 3000
+}
+
+# Standard Egress Rule (Allow all outbound traffic)
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
